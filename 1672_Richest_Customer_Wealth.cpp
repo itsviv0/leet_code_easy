f568901dda09e1041c32744c05af1f22afdca04c
Vivek Sindagi <vivekganeshs12@gmail.com>
2024-04-01 17:55:55 +0530

    Create 1672_Richest_Customer_Wealth.cpp

---
+++ 1672_Richest_Customer_Wealth.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxWealth, curMax=0, i, j;
+        for ( i=0; i<accounts.size(); i++ )
+        {
+            maxWealth = 0;
+            for( j=0; j<accounts[i].size(); j++ )
+                maxWealth += accounts[i][j];
+            if ( maxWealth > curMax )
+                curMax = maxWealth;
+        }  
+        return curMax;
+    }
+};
+
+// beats 78.61% runtime and 100% memory
+// https://leetcode.com/problems/richest-customer-wealth/submissions/1088353113/
